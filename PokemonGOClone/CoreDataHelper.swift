d7dc6e2bfea380a3f69d18fe8b209b186a0ed285
skerney <stevekerney@outlook.com>
2017-01-28 00:28:02 -0500

    Added catching pokemon, added pokedex. App completed.

---
+++ PokemonGOClone/CoreDataHelper.swift
@@ -0,0 +1,9 @@
+//
+//  CoreDataHelper.swift
+//  PokemonGOClone
+//
+//  Created by Steve Kerney on 1/24/17.
+//  Copyright © 2017 Steve Kerney. All rights reserved.
+//
+
+import Foundation
---
+++ PokemonGOClone/PKMNAnnotation.swift
@@ -0,0 +1,9 @@
+//
+//  PKMNAnnotation.swift
+//  PokemonGOClone
+//
+//  Created by Steve Kerney on 1/24/17.
+//  Copyright © 2017 Steve Kerney. All rights reserved.
+//
+
+import Foundation
